7a0d11e2f495cd2f2b424ed03810a18ab391b68d
feh-franc0 <fernandofv1110@gmail.com>
2023-06-13 12:15:41 -0300

    funcionalidade de video com limitacao de tempo de 1 minuto, falta por o audio no video

--- video/script.js
+++ video/script.js
@@ -1,44 +1,86 @@
-var video = document.querySelector("#video");
-var startRecord = document.querySelector("#startRecord");
-var stopRecord = document.querySelector("#stopRecord");
-var downloadLink = document.querySelector("#downloadLink");
+// Verifica se o navegador suporta a API da mídia
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  // Configurações da gravação
+  const constraints = { video: true };
 
-window.onload = async function () {
-  stopRecord.style.display = "none";
+  // Elementos HTML
+  const videoElement = document.getElementById('video-preview');
+  const timerElement = document.getElementById('timer');
+  const startButton = document.getElementById('start-button');
+  const stopButton = document.getElementById('stop-button');
+  const recordedVideoElement = document.getElementById('recorded-video');
 
-  videoStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
-  });
+  let mediaRecorder;
+  let remainingTime;
+  let chunks = [];
+  let countdown;
+  let videoData;
 
-  video.srcObject = videoStream;
-  video.muted = true
-};
+  // Atualiza o temporizador
+  function updateTimer() {
+    console.log('remainingTime: ',  remainingTime)
+    stopRecordingIfTimeLimit()
+    timerElement.textContent = remainingTime;
+    remainingTime++
+  }
 
-startRecord.onclick = function () {
-  console.log('startRecord')
-  startRecord.style.display = "none";
-  stopRecord.style.display = "inline";
+  // Inicia a gravação
+  function startRecording() {
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(function (stream) {
+        videoElement.srcObject = stream;
+        mediaRecorder = new MediaRecorder(stream);
+        remainingTime = 0
 
-  mediaRecorder = new MediaRecorder(videoStream);
+        mediaRecorder.ondataavailable = function (e) {
+          chunks.push(e.data);
+        };
 
-  let blob = [];
+        mediaRecorder.onstop = function () {
+          videoData = new Blob(chunks, { type: 'video/webm' });
+          chunks = [];
+          videoElement.srcObject = null;
+          recordedVideoElement.src = URL.createObjectURL(videoData);
+          recordedVideoElement.controls = true;
+        };
 
-  mediaRecorder.addEventListener("dataavailable", function (e) {
-    blob.push(e.data);
-  });
+        mediaRecorder.start();
+        countdown = setInterval(updateTimer, 1000);
+        startButton.disabled = true;
+        stopButton.disabled = false;
+      })
+      .catch(function (error) {
+        console.error('Erro ao acessar a câmera: ', error);
+      });
+  }
 
-  mediaRecorder.addEventListener("stop", function () {
-    var videoLocal = URL.createObjectURL(new Blob(blob));
-    downloadLink.href = videoLocal;
-  });
+  // Para a gravação
+  function stopRecording() {
+    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+      mediaRecorder.stop();
+      clearInterval(countdown);
+      timerElement.textContent = '0';
+      remainingTime = 0
+      startButton.disabled = false;
+      stopButton.disabled = true;
+    }
+  }
 
-  mediaRecorder.start();
-};
+  // Parar  gravacao com 60 segundos de video
+  function stopRecordingIfTimeLimit() {
+    if (remainingTime === 60) {
+      mediaRecorder.stop();
+      clearInterval(countdown);
+      timerElement.textContent = '0';
+      remainingTime = 0
+      startButton.disabled = false;
+      stopButton.disabled = true;
+    }
+  }
 
-// * --- / / ---
-
-stopRecord.onclick = function () {
-  console.log('stopRecord')
-  mediaRecorder.stop();
-};
+  // Event listeners dos botões
+  startButton.addEventListener('click', startRecording);
+  stopButton.addEventListener('click', stopRecording);
+} else {
+  console.error('O navegador não suporta a API de mídia necessária.');
+}
