5671f86300221d74109c77d4b727285e392bb067
feh-franc0 <fernandofv1110@gmail.com>
2023-06-15 16:52:50 -0300

    adicionando uma camera que fica ativa independente de se estiver ou nao gravando

--- video/script.js
+++ video/script.js
@@ -1,3 +1,19 @@
+var video = document.querySelector(".videoFoto");
+
+// * Ativação da câmera
+document.addEventListener('DOMContentLoaded', function() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      videoStream = stream;
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 // Verifica se o navegador suporta a API da mídia
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   // Configurações da gravação
@@ -34,36 +50,62 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     timerElement.textContent = segundosParaMinutos(remainingTime);
     remainingTime++
   }
-
+  
   // Inicia a gravação
   function startRecording() {
+    // Obtém acesso à câmera usando a função getUserMedia do objeto navigator.mediaDevices
     navigator.mediaDevices.getUserMedia(constraints)
       .then(function (stream) {
+        // Atribui o objeto de stream retornado à propriedade srcObject do elemento de vídeo
         videoElement.srcObject = stream;
-        mediaRecorder = new MediaRecorder(stream);
-        remainingTime = 0
 
+        // Cria um novo objeto MediaRecorder e o associa ao objeto de stream
+        mediaRecorder = new MediaRecorder(stream);
+
+        // Variável para armazenar o tempo restante
+        remainingTime = 0;
+
+        // Define um evento ondataavailable para capturar os dados gravados pelo MediaRecorder
         mediaRecorder.ondataavailable = function (e) {
+          console.log(e.data)
           chunks.push(e.data);
         };
 
+        // Define um evento onstop para ser executado quando a gravação for interrompida
         mediaRecorder.onstop = function () {
+          // Cria um objeto Blob a partir dos chunks de dados gravados, especificando o tipo do arquivo (webm no caso)
           videoData = new Blob(chunks, { type: 'video/webm' });
+
+          // Limpa o array de chunks para a próxima gravação
           chunks = [];
+
+          // Remove o objeto de stream do elemento de vídeo
           videoElement.srcObject = null;
+
+          // Atribui a URL do Blob ao elemento de vídeo usado para reproduzir o vídeo gravado
           recordedVideoElement.src = URL.createObjectURL(videoData);
+
+          // Habilita os controles de reprodução do vídeo gravado
           recordedVideoElement.controls = true;
         };
 
+        // Inicia a gravação
         mediaRecorder.start();
+
+        // Configura um intervalo de 1 segundo para atualizar o tempo restante
         countdown = setInterval(updateTimer, 1000);
+
+        // Desabilita o botão de iniciar a gravação
         startButton.disabled = true;
+
+        // Habilita o botão de parar a gravação
         stopButton.disabled = false;
       })
       .catch(function (error) {
         console.error('Erro ao acessar a câmera: ', error);
       });
   }
+
 
   // Para a gravação
   function stopRecording() {
